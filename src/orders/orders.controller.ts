d1c153414c3e77974f0132d55e530b99cf0817c7
Facundo Petri <facundo.petri@intive.com>
2024-09-01 01:08:39 -0300

    feat: Add generatePdf endpoint to OrdersController
    The code changes in `orders.controller.ts` add a new endpoint `generatePdf` to the `OrdersController`. This endpoint generates a PDF file containing a list of orders and their information. The PDF is generated using the `generatePdf` function from the `helpers/handlebars` module. The endpoint sets the necessary headers for the response to be treated as a PDF file attachment. The PDF generation is handled by the `PdfService` class. This addition allows users to download a PDF file with a list of orders.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,16 +1,68 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Header,
+  Res,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Response } from 'express';
 import { ParseObjectIdPipe } from 'src/pipes/parse-object-id-pipe.pipe';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/schemas/user.schema';
+import { generatePdf } from 'helpers/handlebars';
+import { PdfService } from 'src/pdf/pdf.service';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    private readonly ordersService: OrdersService,
+    private readonly pdfService: PdfService,
+  ) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
+  }
+
+  @Get('generate-pdf')
+  @Header('Content-Type', 'application/pdf')
+  @Header('Content-Disposition', 'attachment; filename="supplies.pdf"')
+  async generatePdf(
+    @Res() res: Response,
+    @CurrentUser() user: User,
+  ): Promise<void> {
+    const orders = await this.ordersService.findAll();
+
+    const html = generatePdf({
+      title: 'Listado de pedidos a proveedores',
+      user: user.fullname,
+      data: orders,
+      headers: [
+        'Número de pedido',
+        'Fecha de creación',
+        'Proveedor',
+        'Insumos',
+      ],
+      tableTemplate: 'orders',
+    });
+
+    const buffer = await this.pdfService.generate(html);
+
+    res.set({
+      'Content-Length': buffer.length,
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0,
+    });
+
+    res.end(buffer);
   }
 
   @Get()
@@ -24,7 +76,10 @@ export class OrdersController {
   }
 
   @Patch(':id')
-  update(@Param('id', ParseObjectIdPipe) id: string, @Body() updateOrderDto: UpdateOrderDto) {
+  update(
+    @Param('id', ParseObjectIdPipe) id: string,
+    @Body() updateOrderDto: UpdateOrderDto,
+  ) {
     return this.ordersService.update(id, updateOrderDto);
   }
 
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,12 +3,13 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/orders.schema';
+import { PdfService } from 'src/pdf/pdf.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, PdfService],
 })
 export class OrdersModule {}
