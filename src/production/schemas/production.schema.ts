fdebee12f0a7cbbad707c71ec8dc5abdd5812399
Facundo Petri <facundo.petri@intive.com>
2024-12-09 10:39:40 -0300

    Enhance Production schema: add unique order number field with auto-increment functionality

--- src/production/schemas/production.schema.ts
+++ src/production/schemas/production.schema.ts
@@ -1,8 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, HydratedDocument } from 'mongoose';
 import { Machine } from 'src/machines/schemas/machine.schema';
 import { Recipe } from 'src/recipes/schemas/recipe.schema';
 import { User } from 'src/users/schemas/user.schema';
+
+export type ProductionDocument = HydratedDocument<Production>;
 
 @Schema()
 export class Production extends Document {
@@ -12,7 +14,7 @@ export class Production extends Document {
   @Prop({ required: true, type: 'ObjectId', ref: 'User' })
   user: User;
 
-  @Prop()
+  @Prop({ unique: true, default: 1 })
   number: number;
 
   @Prop({ required: false })
@@ -35,3 +37,16 @@ export class Production extends Document {
 }
 
 export const ProductionSchema = SchemaFactory.createForClass(Production);
+
+ProductionSchema.statics.getNextOrderNumber = async function () {
+  const lastProd = await this.findOne().sort('-number');
+  return lastProd ? lastProd.number + 1 : 1;
+};
+
+ProductionSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    const doc = this as ProductionDocument;
+    doc.number = await (this.constructor as any).getNextOrderNumber();
+  }
+  next();
+});
